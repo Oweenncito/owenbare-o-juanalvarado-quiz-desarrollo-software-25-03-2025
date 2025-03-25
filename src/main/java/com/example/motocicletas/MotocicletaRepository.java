cc532bfb3a30a13f649bb26cc5eddfc989735f46
juanes1724 <alvarado.juan.1724@ema.edu.co>
2025-03-25 08:38:04 -0500

    JUAN ALVARADO creado clase MotocicletaRepository

---
+++ src/main/java/com/example/motocicletas/MotocicletaRepository.java
@@ -0,0 +1,41 @@
+package com.example.motocicletas;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+public class MotocicletaRepository {
+    private final Map<String, Motocicleta> baseDeDatos = new HashMap<>();
+
+    // Guardar una motocicleta
+    public Motocicleta save(Motocicleta motocicleta) {
+        baseDeDatos.put(motocicleta.getId(), motocicleta);
+        return motocicleta;
+    }
+
+    // Encontrar motocicleta por ID
+    public Motocicleta findById(String id) {
+        return baseDeDatos.get(id);
+    }
+
+    // Listar todas las motocicletas
+    public List<Motocicleta> findAll() {
+        return new ArrayList<>(baseDeDatos.values());
+    }
+
+    // Eliminar una motocicleta
+    public void deleteById(String id) {
+        baseDeDatos.remove(id);
+    }
+
+    // Actualizar las motocicletas
+    public Motocicleta update(Motocicleta motocicleta) {
+        if (baseDeDatos.containsKey(motocicleta.getId())) {
+            baseDeDatos.put(motocicleta.getId(), motocicleta);
+            return motocicleta;
+        }
+        return null;
+
+    }
+}
