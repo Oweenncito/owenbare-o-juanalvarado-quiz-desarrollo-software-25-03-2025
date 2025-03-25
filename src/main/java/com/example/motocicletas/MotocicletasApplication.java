7a7d8552f5b6d3428bc35cce6d6a5109711f96c2
Oweenncito <owenfernando08@gmail.com>
2025-03-25 07:40:11 -0500

    primer commit

---
+++ src/main/java/com/example/motocicletas/MotocicletasApplication.java
@@ -0,0 +1,13 @@
+package com.example.motocicletas;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MotocicletasApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MotocicletasApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/motocicletas/MotocicletasApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.motocicletas;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MotocicletasApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
